b2b6b60761abcb4ebe6125efbe33da9ff50a4bd6
HariNyathani <harinyathani10@gmail.com>
2026-06-12 13:40:35 +0530

    Implement Kadane's Algorithm for maximum subarray sum

---
+++ 3_Arrays/Medium/4_Kadane_Algorithm.py
@@ -0,0 +1,14 @@
+def kadane(nums):
+  max_sum = nums[0]
+  curr_sum = 0
+  for num in nums:
+    curr_sum += num
+    if curr_sum > max_sum:
+      max_sum = curr_sum
+    if curr_sum < 0:
+      curr_sum = 0
+
+  return max_sum
+
+nums = [-2,1,-3, 4,-1,2,1,-5,4]
+kadane(nums)
