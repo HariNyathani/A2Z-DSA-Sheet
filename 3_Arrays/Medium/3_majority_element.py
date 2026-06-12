819eb5f37440a769b4014ddcb9922e50910a01d7
HariNyathani <harinyathani10@gmail.com>
2026-06-12 13:38:25 +0530

    Optimal majority function to Major_ele

--- 3_Arrays/Medium/3_majority_element.py
+++ 3_Arrays/Medium/3_majority_element.py
@@ -1,6 +1,16 @@
-def majority(nums):
-    nums.sort()
-    return nums[len(nums)//2]
+def Major_ele(nums):
+  candidate = 0
+  count = 0
+  for num in nums:
+    if count == 0:
+      candidate = num
+      count += 1
+    elif num == candidate:
+      count += 1
+    else:
+      count -= 1
+  return candidate
 
-nums = [9, 2, 9, 9, 5, 9, 1]
-print(majority(nums))
+
+arr = [9, 2, 9, 9, 5, 9, 1]
+print(Major_ele(arr))
