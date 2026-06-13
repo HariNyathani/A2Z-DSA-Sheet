2add263f09bf6af1bc1276c70f58ed82224baa7f
HariNyathani <harinyathani10@gmail.com>
2026-06-13 23:58:27 +0530

    Implement function to find longest consecutive sequence

---
+++ 3_Arrays/Medium/9_longest_cons_seq.py
@@ -0,0 +1,19 @@
+def longest_con_seq(nums):
+  num_set = set(nums)
+  longest_streak = 1
+  
+  for i in nums:
+    if i-1 not in num_set:
+      current_num = i
+      current_streak = 1
+    while current_num + 1 in num_set:
+      current_streak += 1
+      current_num += 1
+
+    if longest_streak < current_streak:
+      longest_streak = current_streak
+  
+  return longest_streak
+
+nums = [100, 4, 200, 1, 3, 2]  
+longest_con_seq(nums)
