5d7659b2f56c19b24a7f0b5e4fcd93b9c0859bd4
HariNyathani <harinyathani10@gmail.com>
2026-06-13 12:55:03 +0530

    Implement optimal buy and sell stock function
    Added an optimal single pass approach for stock buy and sell problem.

--- 3_Arrays/Medium/5_Stock_buy_and_sell.py
+++ 3_Arrays/Medium/5_Stock_buy_and_sell.py
@@ -10,3 +10,20 @@ def stock(nums):
 
 arr = [7, 1, 5, 3, 6, 4]
 stock(arr)
+
+### Optiomal Single pass approach
+
+def buy_and_sell(nums):
+  min_price = nums[0]
+  max_profit = 0
+  for i in nums:
+    if i < min_price:
+      min_price = i
+    else:
+      profit = i - min_price
+      if max_profit < profit:
+        max_profit = profit
+  return max_profit
+
+arr = [7, 1, 5, 3, 6, 4]
+buy_and_sell(arr)
