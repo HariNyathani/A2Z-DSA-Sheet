0eee536ce525344b48a0f05b253a8a61e6125fc9
HariNyathani <harinyathani10@gmail.com>
2026-06-14 00:17:47 +0530

    Refactor longest_con_seq to longestConsecutive

--- 3_Arrays/Medium/9_longest_cons_seq.py
+++ 3_Arrays/Medium/9_longest_cons_seq.py
@@ -1,19 +1,21 @@
-def longest_con_seq(nums):
-  num_set = set(nums)
-  longest_streak = 1
-  
-  for i in nums:
-    if i-1 not in num_set:
-      current_num = i
-      current_streak = 1
-    while current_num + 1 in num_set:
-      current_streak += 1
-      current_num += 1
+def longestConsecutive(nums):
+    if not nums:
+        return 0
+    num_set = set(nums)
+    longest_streak = 1
 
-    if longest_streak < current_streak:
-      longest_streak = current_streak
-  
-  return longest_streak
+    for i in num_set:
+        if i-1 not in num_set:
+            current_num = i
+            current_streak = 1
+            while current_num + 1 in num_set:
+                current_streak += 1
+                current_num += 1
+
+            if longest_streak < current_streak:
+                longest_streak = current_streak
+
+    return longest_streak
 
 nums = [100, 4, 200, 1, 3, 2]  
 longest_con_seq(nums)
